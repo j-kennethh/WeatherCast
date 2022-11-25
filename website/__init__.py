65d017d906f2cc988540a429028fc43476919e8b
John Kenneth <johnkennethagpaoa@gmail.com>
2022-11-25 20:13:10 +0800

    created basic HTML layout

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create():
     app = Flask(__name__)
 
     @app.route('/')
     def index():
-        return '<h1>Weatherlens</h1>'
+        return render_template('layout.html')
 
     return app
