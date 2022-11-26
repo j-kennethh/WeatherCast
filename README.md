0c1f522e1b8e98ec4c6aacca2f61fcdf167b28ea
John Kenneth <johnkennethagpaoa@gmail.com>
2022-11-26 20:57:56 +0800

    implemented geocoding

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Weatherlens
 A weather web application created using **Flask** and **OpenWeather's API**.
+
+---
+
+## Installation
+Use  [pip](https://pip.pypa.io/en/stable/) to install the packages `flask` and `requests`.
+```
+$ pip install flask
+```
+```
+$ pip install requests
+```
--- website/view.py
+++ website/view.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, redirect, url_for, render_template
+import requests
 
 view = Blueprint('view', __name__)
 
@@ -6,6 +7,15 @@ view = Blueprint('view', __name__)
 @view.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        pass
+        city = request.form.get('city')
+        limit = 1
+        API_KEY = 'fa8939ac34315fc99e926f80094f1da3'
+
+        geo_api = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit={limit}&appid={API_KEY}'
+        geo_res = requests.get(geo_api).json()[0]
+        name, country, lat, lon = geo_res['name'], geo_res['country'], geo_res['lat'], geo_res['lon']
+
+        print(f'City: {name}\nCountry: {country}\nLatitude: {lat}\nLongitude: {lon}')
+        return redirect(url_for('view.index'))
     else: 
         return render_template('index.html')
